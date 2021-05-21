f6890ee091415dfbeac85370747ae2b2ae3dbd86
Luiz Matias <luiz.matias1999@gmail.com>
2021-05-21 06:06:21 -0300

    FEAT: Configured startup of splash screen

---
+++ android/app/src/main/java/br/com/warren/challenge/app/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package br.com.warren.challenge.app.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.warren.challenge.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
