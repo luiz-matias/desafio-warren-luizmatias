e6cf96f55044fafe36c9b13a69f52c5ca4cb5292
Luiz Matias <luiz.matias1999@gmail.com>
2021-05-21 13:01:07 -0300

    FEAT: Redirection to login added

---
+++ android/app/src/main/java/br/com/warren/challenge/app/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.warren.challenge.app.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.warren.challenge.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
