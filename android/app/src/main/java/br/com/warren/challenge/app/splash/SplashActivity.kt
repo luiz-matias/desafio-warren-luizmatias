cb2c38c22c8194fc5a2fd7942bc90ae317ae0294
Luiz Matias <luiz.matias1999@gmail.com>
2021-05-24 13:20:15 -0300

    FEAT: Implemented persistence validation into splash screen

--- android/app/src/main/java/br/com/warren/challenge/app/splash/SplashActivity.kt
+++ android/app/src/main/java/br/com/warren/challenge/app/splash/SplashActivity.kt
@@ -7,15 +7,27 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import br.com.warren.challenge.R
 import br.com.warren.challenge.app.login.LoginActivity
+import br.com.warren.challenge.app.portfolio.PortfolioActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel: SplashViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+
+            if (viewModel.isUserLogged()) {
+                startActivity(Intent(this@SplashActivity, PortfolioActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            }
+
             finish()
+
         }, 2000)
 
     }
