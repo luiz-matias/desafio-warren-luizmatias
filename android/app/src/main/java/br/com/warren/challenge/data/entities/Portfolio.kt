ec16639fa66c9f326336e309a1da3ed7c6c3ba9a
Luiz Matias <luiz.matias1999@gmail.com>
2021-05-24 05:39:22 -0300

    FIX: Changed currencies from double to bigdecimal

--- android/app/src/main/java/br/com/warren/challenge/data/entities/Portfolio.kt
+++ android/app/src/main/java/br/com/warren/challenge/data/entities/Portfolio.kt
@@ -1,12 +1,13 @@
 package br.com.warren.challenge.data.entities
 
 import com.google.gson.annotations.SerializedName
+import java.math.BigDecimal
 
 data class Portfolio(
     @SerializedName("_id") val _id: String,
     @SerializedName("name") val name: String,
     @SerializedName("background") val background: Background,
-    @SerializedName("totalBalance") val totalBalance: Double,
-    @SerializedName("goalAmount") val goalAmount: Int,
+    @SerializedName("totalBalance") val totalBalance: BigDecimal,
+    @SerializedName("goalAmount") val goalAmount: BigDecimal?,
     @SerializedName("goalDate") val goalDate: String
 )
