3a9975459564a140f2f8be12b991c296f3704571
Luiz Matias <luiz.matias1999@gmail.com>
2021-05-24 02:05:27 -0300

    FEAT: Created portfolio activity

---
+++ android/app/src/main/java/br/com/warren/challenge/app/portfolio/PortfolioActivity.kt
@@ -0,0 +1,12 @@
+package br.com.warren.challenge.app.portfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.warren.challenge.R
+
+class PortfolioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portfolio)
+    }
+}
